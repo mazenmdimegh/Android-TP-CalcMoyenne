570475ecba7a4b45e6c9f7297f8c78acd13e2609
mazenmdimegh <mazenmdimagh@gmail.com>
2022-02-15 16:30:31 +0100

    Sms application

--- app/src/main/java/com/moneyconverter/calculmoyenne/ActivityEchec.java
+++ app/src/main/java/com/moneyconverter/calculmoyenne/ActivityEchec.java
@@ -3,10 +3,13 @@ package com.moneyconverter.calculmoyenne;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class ActivityEchec extends AppCompatActivity {
+    private String msg;
     private TextView Moy;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +18,22 @@ public class ActivityEchec extends AppCompatActivity {
         Intent intent =getIntent();
         this.Moy= findViewById(R.id.Dommage);
         if(intent.hasExtra("Moyenne")){
+            msg= "Dommage, vous ayez échoué avec une moyenne de  "+intent.getStringExtra("Moyenne");
             Moy.setText("Dommage, vous ayez échoué avec une moyenne de  "+intent.getStringExtra("Moyenne"));
         }
     }
+    public void backHome(View v){
+        Intent intent= new Intent(this,MainActivity.class);
+        startActivity(intent);
+    }
+    public void SendSms(View v){
+        Intent i = new Intent();
+        i.setAction(Intent.ACTION_SENDTO);
+        Uri uri = Uri.parse("smsto:54310454");
+        i.setData(uri);
+        i.putExtra("sms_body",this.msg);
+        startActivity(i);
+    }
+
+
 }
--- app/src/main/java/com/moneyconverter/calculmoyenne/Activity_Reussi.java
+++ app/src/main/java/com/moneyconverter/calculmoyenne/Activity_Reussi.java
@@ -3,12 +3,14 @@ package com.moneyconverter.calculmoyenne;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
 import android.widget.TextView;
 
 public class Activity_Reussi extends AppCompatActivity {
-
+    private String msg ;
     private TextView Moy;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,13 +21,22 @@ public class Activity_Reussi extends AppCompatActivity {
         Intent intent =getIntent();
         this.Moy= findViewById(R.id.Felicitation);
         if(intent.hasExtra("Moyenne")){
+            msg = "Féliciation vous avez réussi avec une moyenne de "+intent.getStringExtra("Moyenne");
             Moy.setText("Féliciation vous avez réussi avec une moyenne de "+intent.getStringExtra("Moyenne"));
         }else {Moy.setText("Rien");}
 
 
     }
-    public void SmsApp(){
-        Intent i = getPackageManager().getLaunchIntentForPackage("com.google.android.apps.messagingLauncher:com.google.android.apps.messaging.ui.ConversationListActivity");
-        startActivity(i);
+    public void backHome(View v){
+        Intent intent= new Intent(this,MainActivity.class);
+        startActivity(intent);
+    }
+    public void SmsApp(View v){
+        Intent i = new Intent();
+        i.setAction(Intent.ACTION_SENDTO);
+        Uri uri = Uri.parse("sms to :54310454");
+        i.setData(uri);
+        i.putExtra("sms_body",msg);
+                startActivity(i);
     }
 }
